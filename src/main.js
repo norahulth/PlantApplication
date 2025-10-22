c5fb6b06cc4a4b13100bbe0c6bb906aaf3b77728
norahulth <nora.hulth@gmail.com>
2025-10-22 11:31:45 +0200

    gitignore added

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { registerSW } from 'virtual:pwa-register';
 import { createApp } from "vue";
 import App from "./App.vue";
+import { ensurePushSubscribed } from './push';
 import router from "./router";
 import store from "./store";
 
-const updateSW = registerSW({
+registerSW({
+  immediate: true,
   onNeedRefresh() {},
   onOfflineReady() {}
 })
 
 createApp(App).use(store).use(router).mount("#app");
+
+ensurePushSubscribed()
